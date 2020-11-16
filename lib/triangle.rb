6d47f3940db840b5c3175e4ef86b92207ae7acad
Mike Wilder <mikewilder01@gmail.com>
2020-11-16 18:27:46 -0500

    commit tree ea615d860ae0c22e6abb64fbcb6b9d6187fcdf25

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,37 @@
 class Triangle
   # write code here
+  def initialize(a, b, c)
+
+    if a >= (b + c) || b >= (a + c) || c >= (a + b)
+      raise TriangleError
+    end
+
+    if a <= 0 || b <= 0 ||c <= 0
+      raise TriangleError
+    end
+
+    if a == nil || b == nil ||c== nil
+      raise TriangleError
+    end
+
+    @a = a 
+    @b = b 
+    @c = c 
+  end
+
+  def kind
+    if @a == @b && @b == @c 
+      return :equilateral 
+    elsif @a == @b || @b == @c || @a == @c
+      return :isosceles 
+    elsif @a != @b && @b != @c && @a != @c 
+      return :scalene
+    end 
+    end
+    class TriangleError < StandardError 
+      def message 
+        "This is not a valid triangle."
+      end
+  end
 end
+
